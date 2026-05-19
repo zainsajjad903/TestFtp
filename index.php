5a879293e0fe861b57628f8ab6c0dffd45277871
zainsajjad903 <zainsajjad.903@gmail.com>
2026-05-20 00:33:49 +0500

    setup auto deploy

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$name = "Ali";
+$course = "PHP Testing";
+
+echo "<h1>PHP Test Page</h1>";
+echo "Name: " . $name . "<br>";
+echo "Course: " . $course . "<br>";
+echo "Server Time: " . date("Y-m-d H:i:s");
+
+?>  
